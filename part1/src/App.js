4d01b4a4e71d7a3420650dd975faf9668580fe24
Burak Tilek <burak.tilek@derivco.ee>
2021-09-08 15:44:57 +0300

    Add course information step1 solution

---
+++ part1/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Header = (props) => (
+  <h1>{props.course}</h1>
+)
+
+const Content = (props) => (
+  <p>
+    {props.part} {props.exercises}
+  </p>
+)
+
+const Footer = (props) => (
+  <p>Number of exercises {props.exercises.reduce((a, b) => a + b)}</p>
+)
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Footer exercises={[exercises1, exercises2, exercises3]} />
+    </div>
+  )
+}
+
+export default App;
