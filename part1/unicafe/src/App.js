e4a502bd61bc64fddbd49273821254e4adb89a9a
Burak Tilek <burak.tilek@derivco.ee>
2021-09-11 14:18:17 +0300

    Add unicafe step1 solution

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Header = (props) => (
+  <h1>{props.header}</h1>
+)
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Display = ({ info }) => <p>{info}</p>
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header header={'Give feedback'} />
+      <Button onClick={() => setGood(good + 1)} text={"good"} />
+      <Button onClick={() => setNeutral(neutral + 1)} text={"neutral"} />
+      <Button onClick={() => setBad(bad + 1)} text={"bad"} />
+      <Header header={'Statistics'} />
+      <Display info={`good ${good}`} />
+      <Display info={`neutral ${neutral}`} />
+      <Display info={`bad ${bad}`} />
+    </div>
+  )
+}
+
+export default App;
