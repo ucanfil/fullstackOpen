f631ed4594113e4044d452ea77bfedd04fa184c6
Burak Tilek <burak.tilek@derivco.ee>
2021-09-11 15:02:58 +0300

    Add unicafe step4 solution

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,16 +9,20 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 const Statistics = ({good, neutral, bad}) => {
   const all = good + neutral + bad;
 
-  return (
-    <>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average ${good - bad === 0 ? 0 : (good - bad) / all}</p>
-      <p>positive ${good === 0 ? 0 : good / (all) * 100} %</p>
-    </>
-  )
+  if (good || neutral || bad) {
+    return (
+      <>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average ${good - bad === 0 ? 0 : (good - bad) / all}</p>
+        <p>positive ${good === 0 ? 0 : good / (all) * 100} %</p>
+      </>
+    )
+  }
+
+  return <p>No feedback given</p>;
 }
 
 const App = () => {
