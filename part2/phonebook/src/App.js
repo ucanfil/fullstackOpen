21ef335db19d97beffbc357f27bb0ade8c4adb85
Burak Tilek <burak.tilek@derivco.ee>
2021-09-14 22:25:41 +0300

    Add phonebook step6 solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Person from './components/Person';
 import PersonsForm from './components/PersonsForm';
 import Search from './components/Search';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
-
+  const [persons, setPersons] = useState([]);
+  const [notes, setNotes] = useState([]);
   const [filterBy, setFilterBy] = useState('');
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
+
+  useEffect(() => {
+    console.log("effect");
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log("Promise fullfilled!");
+        setPersons(response.data);
+      });
+  }, []);
+
+  console.log(`render ${persons.length} persons`);
 
   const handleFilterBy = (e) => setFilterBy(e.target.value.toLowerCase());
 
