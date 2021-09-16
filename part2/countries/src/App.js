eb0d7824e10a3a038333363446840b56f8112f64
Burak Tilek <burak.tilek@derivco.ee>
2021-09-16 14:18:21 +0300

    Add countries step3 solution

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,16 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const Country = ({ country }) => (
-  <div>
-    <h1>{country.name}</h1>
-    <p>capital {country.capital}</p>
-    <p>population {country.population}</p>
-    <h2>languages</h2>
-    {country.languages.map(language => <p key={language.name}>{language.name}</p>)}
-    <img src={country.flag} alt="country_flag" width="150"></img>
-  </div>
-)
+const Country = ({ country }) => {
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    let isActive = true;
+
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`)
+      .then(response => {
+        setWeather(response.data);
+      });
+
+    return () => {
+      isActive = false;
+    };
+  },[country]);
+
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <p>capital {country.capital}</p>
+      <p>population {country.population}</p>
+      <h2>languages</h2>
+      {country.languages.map(language => <p key={language.name}>{language.name}</p>)}
+      <img src={country.flag} alt="country_flag" width="150"></img>
+      {weather && weather.main && (
+        <>
+          <h2>Weather in {country.capital}</h2>
+          <p><strong>temperature:</strong> {weather.main.temp} Celcius</p>
+          <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}></img>
+          <p><strong>wind:</strong> {weather.wind.speed} mph degree {weather.wind.deg}</p>
+        </>)
+      }
+    </div>
+  );
+}
 
 function App() {
   const [countries, setCountries] = useState([]);
